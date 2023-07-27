f55139a72d1011630836642bd0d7504a6862fbad
Maxim N <mnosov622@gmail.com>
2023-07-27 09:16:07 -0400

    async components added

--- src/router.js
+++ src/router.js
@@ -1,12 +1,22 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import Auth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachDetail.vue')
+);
+const CoachRegistration = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachRegistration.vue')
+);
+const ContactCoach = defineAsyncComponent(() =>
+  import('./pages/requests/ContactCoach.vue')
+);
+const RequestReceived = defineAsyncComponent(() =>
+  import('./pages/requests/RequestReceived.vue')
+);
+const Auth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'));
 
 const route = createRouter({
   history: createWebHistory(),
